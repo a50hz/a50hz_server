98c2b45ba0b7d4caf0e70c553b57fd3b898b0fae
Fidanze <fidanze@gmail.com>
2021-01-13 15:27:11 +0500

    made using db data

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import geojsoncontour
+from django.db import connection
 from main.models import Measurement
 
 
-def get_isolines(x1, x2, y1, y2):
+def pget_isolines(x1, x2, y1, y2):
     data = get_data(x1, x2, y1, y2)
     step = min((x1-x2)/1000, (y1-y2)/1000)
     lon_range, lat_range = np.mgrid[x2:x1:step, y2:y1:step] # 6 знаков после запятой
@@ -20,9 +21,10 @@ def get_isolines(x1, x2, y1, y2):
     return geojson
 
 
-def g1et_isolines(x1, x2, y1, y2):
+def get_isolines(x1, x2, y1, y2):
+    #data =  my_custom_sql("SELECT * FROM public.main_measurement WHERE ({} < longitude) and (longitude < {}) and ({} < latitude) and (latitude < {})".format((lon_array[i]-step), (lon_array[i]+step), (lat_array[j]-step), (lat_array[j]+step)))
+    step = max((x1-x2)/1000, (y1-y2)/1000)
     data = get_data(x1, x2, y1, y2)
-    step = min((x1-x2)/1000, (y1-y2)/1000)
     lon_array = [i for i in np.arange(x2,x1,step)]
     lat_array = [i for i in np.arange(y2,y1,step)]
 
@@ -30,13 +32,20 @@ def g1et_isolines(x1, x2, y1, y2):
         (len(lon_array), len(lat_array))
     )
 
-    for id, value in enumerate(point_grid):
-        for j in value:
-            data = Measurement.objects.raw("SELECT * FROM public.main_measurement WHERE ({} < longitude) and (longitude < {}) and ({} < latitude) and (latitude < {})".format((lon_array[id]-step), (lon_array[id]+step), (lat_array[id]-step), (lat_array[id]+step)))
+    for i in range(len(lon_array)):
+        for j in range(len(lat_array)):
+            
+            if data != None:
+                point_grid[i][j] = data
+        print(i)
+
+    print(point_grid == np.zeros(
+        (len(lon_array), len(lat_array))
+    ))
 
     figure = plt.figure()
     ax = figure.add_subplot(111)
-    contour = ax.contour(lon_array, lat_array, Z, level=16, cmap=plt.cm.jet)
+    contour = ax.contour(lon_array, lat_array, point_grid, level=16, cmap=plt.cm.jet)
     geojson = geojsoncontour.contour_to_geojson(
         contour=contour,
         ndigits=3,
@@ -46,11 +55,10 @@ def g1et_isolines(x1, x2, y1, y2):
 
 
 def get_heatmap(x1, x2, y1, y2):
-    data = get_data(x1, x2, y1, y2)
     len_diag = ((x1 - x2)**2 + (y1 - y2)**2)**.5
-    step_x = (x1-x2)/1000
-    step_y = (y1-y2)/1000
-    lon_range, lat_range = np.mgrid[x2:x1:step_x, y2:y1:step_y] # 6 знаков после запятой
+    step = max((x1-x2)/1000, (y1-y2)/1000)
+    data = get_data(x1, x2, y1, y2, step)
+    lon_range, lat_range = np.mgrid[x2:x1:step, y2:y1:step] # 6 знаков после запятой
     Z = np.sinc(lon_range) * np.cos(lat_range)
     n_contours = 20
     levels = np.linspace(start=0, stop=100, num=n_contours)
@@ -65,6 +73,12 @@ def get_heatmap(x1, x2, y1, y2):
     return geojson
 
 
-def get_data(x1, x2, y1, y2):
-    data = list(Measurement.objects.all())
-    return [i for i in data if x1 >= i.longitude >= x2 and y1 >= i.latitude >= y2]
+def my_custom_sql(self):
+    with connection.cursor() as cursor:
+        cursor.execute(self)
+        row = cursor.fetchone()
+
+    return row
+
+def get_data(x1, x2, y1, y2, step):
+    return  my_custom_sql("SELECT id, data, longitude, latitude FROM public.main_measurement WHERE ({} < longitude) and (longitude < {}) and ({} < latitude) and (latitude < {})".format((x1-step), (x2+step), (y1-step), (y2+step)))
